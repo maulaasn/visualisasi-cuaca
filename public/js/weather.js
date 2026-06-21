c722ffc09bba28eda477536f867350eee318bd29
bitterswag <maula.shahihah@gmail.com>
2026-06-21 17:12:14 +0700

    update file opacity layer cuaca

--- public/js/weather.js
+++ public/js/weather.js
@@ -414,7 +414,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 color: 'white',
                 dashArray: '',
                 opacity: 1,
-                fillOpacity: 0.8,
+                fillOpacity: 0.85,
             });
         } else {
             layer.setStyle({
